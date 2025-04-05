069c177012d72f63811523f91f073c2547eecf7d
CHRIST <1609332741@qq.com>
2025-04-05 21:23:27 +0800

    Create keymap.lua

---
+++ keymap.lua
@@ -0,0 +1,32 @@
+Keymap = {}
+
+Keymap.MODE = {
+    NORMAL = 'n',
+    INSERT = 'i',
+    VISUAL = 'v',
+    COMMAND = 'c',
+}
+
+Keymap.validateMode = function(mode)
+    for key in pairs(Keymap.MODE) do
+        if key == mode then
+            return true
+        end
+    end
+    error("Invalid mode: " .. mode .. ". Expected one of: " .. table.concat(Keymap.MODE, ", "))
+end
+
+Keymap.whenInMode = function(mode)
+    Keymap.validateMode(mode)
+    local map = function(key)
+        local to = function(keyOrCommand)
+            vim.keymap.set(Keymap.MODE[mode], key, keyOrCommand)
+        end
+        return { to = to }
+    end
+    return {
+        map = map,
+    }
+end
+
+return Keymap
